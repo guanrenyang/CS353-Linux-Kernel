be9c5015f60a24dde778e1c4ed3e4d0f4e4bcfc0
guanrenyang <renyangguan@outlook.com>
2022-04-02 18:00:21 +0800

    modify

--- project1/calc.c
+++ project1/calc.c
@@ -20,96 +20,68 @@ static struct proc_dir_entry *proc_dir;
 static int output[MAX_SIZE];
 int out_len;
 
-// static ssize_t proc_read(struct file *fp, char __user *ubuf, size_t len, loff_t *pos)
-// {   
-
-//     if(out_len!=0 && *pos>=out_len) {
-//         pr_info("LOG: copy done");
-//         return 0;
-//     }
-
-//     // 判断操作符`operator`，执行对应操作或输出错误信息
-//     if (operator!=NULL&&!strcmp(operator, "add")) {
-//         int i;
-//         for (i = 0; i < ninp; i++)
-//         {
-//             output[i] = operand1+operand2[i];
-//         }
-//     } else if(operator!=NULL&&!strcmp(operator, "mul")) {
-//         int i;
-//         for (i = 0; i < ninp; i++)
-//         {
-//             output[i] = operand1*operand2[i];
-//         }
-//     } else {
-//         pr_err("ERROR:operator is uncorrect!");
-//         return 0;
-//     }
-    
-//     // 将计算出来的结果（int数组）转换为字符串
-//     char output_buffer[10*MAX_SIZE]="";
-    
-//     int i = 0;
-//     for ( i = 0; i < ninp; i++)
-//     {   
-//         char int_buffer[4];
-//         snprintf(int_buffer, 4, "%d", output[i]);
-//         strcat(output_buffer, int_buffer);
-//         if (i!=ninp-1)
-//             strcat(output_buffer, ",");
-//         else
-//             strcat(output_buffer, "\n");
-//     }
-    
-//     // 计算输出字符串的长度
-//     out_len = strlen(output_buffer);
-
-//     // if(*pos>=out_len) {
-//     //     pr_info("LOG: copy done");
-//     //     return 0;
-//     // }
-    
-//     // 将结果从内核空间拷贝到用户空间并自增偏移量`*pos`
-//     int err;
-//     err = copy_to_user(ubuf, output_buffer, out_len); 
-//     if(err){
-//         pr_alert("ERROR: fail to copy to user");
-//     } else {
-//         pr_info("LOG: copy to user");
-//         *pos += out_len;
-//     }
-
-//     return out_len;
-// }
 static ssize_t proc_read(struct file *fp, char __user *ubuf, size_t len, loff_t *pos)
-{
-    /* TODO */
-    if(*pos>=26){
+{   
+
+    if(out_len!=0 && *pos>=out_len) {
+        pr_info("LOG: copy done");
         return 0;
     }
-    pr_info("pos 1:%lld", *pos);
-    char s[13] = "HelloWorld!\n"; 
-    int l = sizeof(s); 
-    ssize_t ret = l; 
-    // pr_info("len: %ld", len);
 
-    if (copy_to_user(ubuf, s, l))
-        pr_info("error");
-    *pos+=l;
-
-    pr_info("pos 2:%lld", *pos);
-
-    if (copy_to_user(ubuf, s, l))
-        pr_info("error");
-    *pos+=l;
-
-    pr_info("pos 3:%lld", *pos);
+    // 判断操作符`operator`，执行对应操作或输出错误信息
+    if (operator!=NULL&&!strcmp(operator, "add")) {
+        int i;
+        for (i = 0; i < ninp; i++)
+        {
+            output[i] = operand1+operand2[i];
+        }
+    } else if(operator!=NULL&&!strcmp(operator, "mul")) {
+        int i;
+        for (i = 0; i < ninp; i++)
+        {
+            output[i] = operand1*operand2[i];
+        }
+    } else {
+        pr_err("ERROR:operator is uncorrect!");
+        return 0;
+    }
     
-    pr_info("pos 4:%lld", *pos);
-    pr_info("pos 5:%lld", *pos);
-    pr_info("pos 6:%lld", *pos);
-    return 13; 
+    // 将计算出来的结果（int数组）转换为字符串
+    char output_buffer[10*MAX_SIZE]="";
+    
+    int i = 0;
+    for ( i = 0; i < ninp; i++)
+    {   
+        char int_buffer[4];
+        snprintf(int_buffer, 4, "%d", output[i]);
+        strcat(output_buffer, int_buffer);
+        if (i!=ninp-1)
+            strcat(output_buffer, ",");
+        else
+            strcat(output_buffer, "\n");
+    }
+    
+    // 计算输出字符串的长度
+    out_len = strlen(output_buffer);
+
+    // if(*pos>=out_len) {
+    //     pr_info("LOG: copy done");
+    //     return 0;
+    // }
+    
+    // 将结果从内核空间拷贝到用户空间并自增偏移量`*pos`
+    int err;
+    err = copy_to_user(ubuf, output_buffer, out_len); 
+    if(err){
+        pr_alert("ERROR: fail to copy to user");
+    } else {
+        pr_info("LOG: copy to user");
+        *pos += out_len;
+    }
+
+    return out_len;
 }
+
 static ssize_t proc_write(struct file *fp, const char __user *ubuf, size_t len, loff_t *pos)
 {
     /* TODO */
@@ -152,12 +124,12 @@ static int __init proc_init(void)
     proc_ent = proc_create(PROC_NAME, 0, proc_dir, &proc_ops);
     if (proc_ent==NULL){
         proc_remove(proc_dir);
-        pr_alert("Error:Could not initialize /proc/%s/%s\n", ID, PROC_NAME); 
+        pr_alert("ERROR:Could not initialize /proc/%s/%s\n", ID, PROC_NAME); 
         return -ENOMEM; 
     }
     
     pr_info("/proc/%s/%s created\n", ID, PROC_NAME);
-
+    
     // 返回`0`, 表示成功初始化模块
 	return 0;
 }
