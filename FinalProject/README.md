885be6826fbf6dc0e67c934c62a3bb0bcb37dca1
Renyang Guan <renyangguan@outlook.com>
2022-06-21 04:47:55 -0700

    all done, except for update_stat

--- FinalProject/README.md
+++ FinalProject/README.md
@@ -23,3 +23,15 @@ When ptep is none, ptep_test_and_clear_young(ptep)=0.
 python无法使用open读取proc文件，试过改proc read函数，但是总是killed。
 
 多写一些qt的thread和process，注重同步机制
+
+![image-20220621181624116](https://michael-picgo.obs.cn-east-3.myhuaweicloud.com/image-20220621181624116.png)
+
+这段代码必须要写在构造函数当中，否则thread启动后，开始的变量被回收了，thread就变成了僵尸线程
+
+在C++的实例中worker是一个pointer，所以可以是临时变量，这样虽然pointer本身会随着构造函数而消失，但是new出来的空间不会消失。而在python中必须要worker和workerthread定义成成员变量（self.），否则都会随着构造函数而解析掉
+
+使用page的问题：
+
+1. 可能在interval内testbench多次读写同一个页，但是我们只计一次
+2. 无法判断内存读写是否连续
+3. 集中读取同一页、与分散读取多个页，无法区分
--- FinalProject/watch.py
+++ FinalProject/watch.py
@@ -1,6 +1,7 @@
-from ast import arg, arguments
-from asyncore import read
-from sqlite3 import connect
+
+from re import S
+from select import select
+from signal import signal
 from time import sleep
 from PyQt5 import QtCore, QtWidgets
 from PyQt5.QtCore import QProcess, QTimer, QObject, QThread
@@ -9,29 +10,30 @@ from PyQt5.QtChart import QChartView
 import sys
 
 from click import command
+from yaml import emit
 
 
-class WatchProcessControl(QObject):
-    # for comminucation with UI
-    resultReady = QtCore.pyqtSignal(dict) # 4 time, 1 num page
-    
+class CPUWatchWorker(QObject):
+    # for comminucation with other classes
+    signal_resultReady = QtCore.pyqtSignal(str) # 4 time, 1 num page
+    signal_stopWatch = QtCore.pyqtSignal()
     # for inner use
-    startReadProcessStat = QtCore.pyqtSignal()
+    signal_startReadStat = QtCore.pyqtSignal()
 
-    def __init__(self, watchInterval:int, parent=None):
-        super(WatchProcessControl, self).__init__(parent)
+    def __init__(self, parent=None):
+        super(CPUWatchWorker, self).__init__(parent)
         
         self.pid = None
-        self.watchInterval = watchInterval # in ms
-        self.processRunning = False
+        self.watchInterval = None # in ms
         self.timer = None
         self.testbenchProcess = None
         
-        # connect 
-        self.startReadProcessStat.connect(self.start_watch)
-
+        # # connect 
+        # self.signal_startReadStat.connect(self.start_watch)
         
-    def start_testbench(self, command: str, arguments: list):
+    def start_testbench(self, command: str, arguments: list, watchInterval: int):
+        self.watchInterval = watchInterval
+
         self.testbenchProcess = QProcess()
         self.testbenchProcess.start(command, arguments)
         self.testbenchProcess.finished.connect(self.stop_watch)
@@ -42,47 +44,75 @@ class WatchProcessControl(QObject):
         echoProcProcess = QProcess()
         echoProcProcess.setStandardOutputFile('/proc/watch')
         echoProcProcess.start('echo',[str(self.testbenchProcess.processId())])
-        # echoProcProcess.start('echo',['1'])
         echoProcProcess.waitForFinished()
         
         print("DEBUG: `echo` finished")
         # emit signal
-        self.startReadProcessStat.emit()
+        self.signal_startReadStat.emit()
 
 
-    def read_process_stat(self):
+    def read_stat(self):
         '''read /proc/watch'''
         print("LOG: read process stat")
-        readStatProcess = QProcess(self)
+        readStatProcess = QProcess()
         readStatProcess.start('cat', ['/proc/watch'])
         readStatProcess.waitForFinished()
         try:
-            res = eval(str(readStatProcess.readAll()))
-            self.resultReady.emit(res)
-            print(res)
+            res = str(readStatProcess.readAll())
+            self.signal_resultReady.emit(res)
         except:
             print("ERROR: Kernel Module Error")
-            print(readStatProcess.readAll())
-        
 
+    def stop_watch(self):
+        self.signal_stopWatch.emit()
+
+class CPUWatchController(QObject):
+    signal_startWatch = QtCore.pyqtSignal(str, list, int)
+    signal_resultReady = QtCore.pyqtSignal(str)
+    def __init__(self, parent = None):
+        super(CPUWatchController, self).__init__(parent)
+        
+        self.timer = None
+        self.watchInterval = None
+        self.worker = CPUWatchWorker()
+        self.workerThread = QThread()
+        self.worker.moveToThread(self.workerThread)
+
+        self.workerThread.finished.connect(self.deleteLater)
+        self.signal_startWatch.connect(self.worker.start_testbench)
+        self.worker.signal_resultReady.connect(self.handleResult)
+        self.worker.signal_startReadStat.connect(self.start_watch)
+        self.worker.signal_stopWatch.connect(self.stop_watch)
+
+        self.workerThread.start()
+
+    def start(self, command:str, arguments:list, watchInterval:int):
+        self.watchInterval = watchInterval
+        self.signal_startWatch.emit(command, arguments, watchInterval)
+        
+    def handleResult(self, result: str):
+        self.timer.start(self.watchInterval)
+        self.signal_resultReady.emit(result)
+        
     def start_watch(self):
         print("LOG: start watching")
         self.timer = QTimer(self)
-        self.timer.timeout.connect(self.read_process_stat)
+        self.timer.timeout.connect(self.worker.read_stat)
         self.timer.start(self.watchInterval)
-
-
     def stop_watch(self):
         print("LOG: stop watching")
         QTimer.killTimer(self.timer, self.timer.timerId())
-        self.pid = None
+
+    def __del__(self):
+        self.workerThread.wait()
+        self.workerThread.quit()
 
 class WatchUI(QtWidgets.QWidget):
     startTestbench = QtCore.pyqtSignal(str, list)
     def __init__(self, parent=None):
         super(WatchUI, self).__init__(parent)     
 
-        self.testbenchThread = QThread(self)
+        self.watchController = None
 
         # UI  
         self.resize(800, 500)
@@ -148,17 +178,15 @@ class WatchUI(QtWidgets.QWidget):
         command, arguments, watchInterval = self.tool_parse_command()
 
         # start a new thread to run testbench and watching
-        watchProcessControl = WatchProcessControl(watchInterval, self)
-        watchProcessControl.moveToThread(self.testbenchThread)
+        # watchProcessControl = CPUWatchWorker(watchInterval, self)
+        # watchProcessControl.moveToThread(self.testbenchThread)
+        self.watchController = CPUWatchController(self)
+        self.watchController.start(command, arguments, watchInterval)
 
-        self.testbenchThread.finished.connect(watchProcessControl.deleteLater)
-        self.startTestbench.connect(watchProcessControl.start_testbench)
-        watchProcessControl.resultReady.connect(self.update_stat)
-        print(type(arguments))
-        self.startTestbench.emit(command, arguments)
+        self.watchController.signal_resultReady.connect(self.update_stat)
 
-    def update_stat(self, info: dict):
-        pass
+    def update_stat(self, info: str):
+        print(info)
 
     def tool_parse_command(self):
         content = self.inputCommandLineEdit.text()
@@ -170,29 +198,65 @@ class WatchUI(QtWidgets.QWidget):
         except:
             print("ERROR: command error!")
 
+''''''
+class Worker(QObject):
+    resultReady = QtCore.pyqtSignal(str)
+    def __init__(self, parent=None):
+        super(Worker, self).__init__(parent)
+    def do_work(self):
+        self.resultReady.emit('do work')
 
+        testbenchProcess = QProcess()
+        testbenchProcess.start('sysbench', ['memory', 'run'])
+        testbenchProcess.waitForStarted()
 
+        echoProcess = QProcess()
+        echoProcess.setStandardOutputFile('/proc/watch')
+        echoProcess.start('echo', [str(testbenchProcess.processId())])
+        echoProcess.waitForFinished()
 
+        for s in range(10):
+            readProcess = QProcess()
+            readProcess.start('cat', ['/proc/watch'])
+            readProcess.waitForFinished()
+            # print(readProcess.readAll())
+            self.resultReady.emit(str(readProcess.readAll()))
+            sleep(1)
+        testbenchProcess.waitForFinished
+
+class Controller(QObject):
+    operate = QtCore.pyqtSignal()
+    def __init__(self, parent=None):
+        super(Controller, self).__init__(parent)
+        self.workerThread = QThread()
+
+        self.worker = Worker()
+        self.worker.moveToThread(self.workerThread)
+
+        self.workerThread.finished.connect(self.threadOK)
+        self.operate.connect(self.worker.do_work)
+        self.worker.resultReady.connect(self.result)
+
+        self.workerThread.start()
+
+    def emit(self):
+        self.operate.emit()
+    def result(self, s):
+        print(s)
+    def threadOK(self):
+        self.workerThread.wait()
+        self.workerThread.quit()
+    def display(self):
+        print(self.workerThread.isRunning())
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    # mainWindow = WatchUI()
-    # mainWindow.show()
+    mainWindow = WatchUI()
+    mainWindow.show()
     # watchProcessControl  = WatchProcessControl(1000)
     # watchProcessControl.start_testbench('sysbench', ['memory', 'run'])
-
-    testbenchProcess = QProcess()
-    testbenchProcess.start('sysbench', ['memory', 'run'])
-    testbenchProcess.waitForStarted()
-
-    echoProcess = QProcess()
-    echoProcess.setStandardOutputFile('/proc/watch')
-    echoProcess.start('echo', [str(testbenchProcess.processId())])
-    echoProcess.waitForFinished()
-
-    readProcess = QProcess()
-    readProcess.start('cat', ['/proc/watch'])
-    readProcess.waitForFinished()
-    print(readProcess.readAll())
+    # controller = Controller(app)
+    # controller.emit()
+    
     
     sys.exit(app.exec_())
     
