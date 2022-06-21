d99ea3e1d5f0f52201d5ab27634117cfa417e8b1
Renyang Guan <renyangguan@outlook.com>
2022-06-21 01:26:57 -0700

    temporary store

--- FinalProject/watch.py
+++ FinalProject/watch.py
@@ -1,8 +1,13 @@
+from ast import arg, arguments
+from sqlite3 import connect
+from time import sleep
 from PyQt5 import QtCore, QtWidgets
-from PyQt5.QtCore import QProcess, QTimer, QObject
+from PyQt5.QtCore import QProcess, QTimer, QObject, QThread
 from PyQt5.QtWidgets import QMainWindow, QApplication
 from PyQt5.QtChart import QChartView
 import sys
+
+from click import command
 
 
 class WatchProcessControl(QObject):
@@ -12,31 +17,34 @@ class WatchProcessControl(QObject):
     # for inner use
     startReadProcessStat = QtCore.pyqtSignal()
 
-    def __init__(self, parent=None):
+    def __init__(self, watchInterval:int, parent=None):
         super(WatchProcessControl, self).__init__(parent)
         
         self.pid = None
-        self.watchPeriod = 1000 # in ms
+        self.watchInterval = watchInterval # in ms
         self.processRunning = False
         self.timer = None
-        self.testbenchProcess = QProcess(self)
+        self.testbenchProcess = None
         
         # connect 
         self.startReadProcessStat.connect(self.start_watch)
-        self.testbenchProcess.finished.connect(self.stop_watch)
 
         
     def start_testbench(self, command: str, arguments: list):
-
+        self.testbenchProcess = QProcess()
         self.testbenchProcess.start(command, arguments)
+        self.testbenchProcess.finished.connect(self.stop_watch)
+
         # self.pid = self.testbenchProcess.processId()
         print("DEBUG: ", self.testbenchProcess.processId())
         
-        echoProcProcess = QProcess(self)
+        echoProcProcess = QProcess()
         echoProcProcess.setStandardOutputFile('/proc/watch')
         echoProcProcess.start('echo',[str(self.testbenchProcess.processId())])
+        # echoProcProcess.start('echo',['1'])
         echoProcProcess.waitForFinished()
         
+        print("DEBUG: `echo` finished")
         # emit signal
         self.startReadProcessStat.emit()
 
@@ -53,13 +61,14 @@ class WatchProcessControl(QObject):
             print(res)
         except:
             print("ERROR: Kernel Module Error")
+            print(readStatProcess.readAll())
         
 
     def start_watch(self):
         print("LOG: start watching")
         self.timer = QTimer(self)
         self.timer.timeout.connect(self.read_process_stat)
-        self.timer.start(self.watchPeriod)
+        self.timer.start(self.watchInterval)
 
 
     def stop_watch(self):
@@ -68,8 +77,13 @@ class WatchProcessControl(QObject):
         self.pid = None
 
 class WatchUI(QtWidgets.QWidget):
+    startTestbench = QtCore.pyqtSignal(str, list)
     def __init__(self, parent=None):
-        super(WatchUI, self).__init__(parent)        
+        super(WatchUI, self).__init__(parent)     
+
+        self.testbenchThread = QThread(self)
+
+        # UI  
         self.resize(800, 500)
         self.setWindowTitle('CPUWatch')
         
@@ -83,7 +97,7 @@ class WatchUI(QtWidgets.QWidget):
 
         # layer 2
         self.vlayout2_0 = QtWidgets.QVBoxLayout()
-        self.runButton = QtWidgets.QPushButton(self)
+        self.runButton = QtWidgets.QPushButton('RUN',self)
         self.hlayout1_0.addLayout(self.vlayout2_0)
         self.hlayout1_0.addWidget(self.runButton)
 
@@ -91,12 +105,19 @@ class WatchUI(QtWidgets.QWidget):
         self.vlayout2_2 = QtWidgets.QVBoxLayout()
         self.hlayout1_1.addLayout(self.flayout2_1)
         self.hlayout1_1.addLayout(self.vlayout2_2)
+        
         # layer 3
-        self.inputCommandLabel = QtWidgets.QLabel(self)
+        self.inputCommandLabel = QtWidgets.QLabel('Testbench Command (Shell):', self)
         self.inputCommandLineEdit = QtWidgets.QLineEdit(self)
+        self.testIntervalLabel = QtWidgets.QLabel('Test interval (s):', self)
+        self.testIntervalLineEdit = QtWidgets.QLineEdit(self)
         self.vlayout2_0.addWidget(self.inputCommandLabel)
         self.vlayout2_0.addWidget(self.inputCommandLineEdit)
+        self.vlayout2_0.addWidget(self.testIntervalLabel)
+        self.vlayout2_0.addWidget(self.testIntervalLineEdit)
         
+        self.pidLabel = QtWidgets.QLabel("Pid:", self)
+        self.pidLineEdit  =QtWidgets.QLineEdit(self)
         self.utimeLabel = QtWidgets.QLabel("Usert time:", self)
         self.utimeLineEdit = QtWidgets.QLineEdit(self)
         self.stimeLabel = QtWidgets.QLabel("Kernel time:", self)
@@ -107,6 +128,7 @@ class WatchUI(QtWidgets.QWidget):
         self.memoryLineEdit = QtWidgets.QLineEdit(self)
         self.typeLabel = QtWidgets.QLabel("Type:", self)
         self.typeLineEdit = QtWidgets.QLineEdit(self)
+        self.flayout2_1.addRow(self.pidLabel, self.pidLineEdit)
         self.flayout2_1.addRow(self.utimeLabel, self.utimeLineEdit)
         self.flayout2_1.addRow(self.stimeLabel, self.stimeLineEdit)
         self.flayout2_1.addRow(self.cpuLabel, self.cpuLineEdit)
@@ -114,23 +136,53 @@ class WatchUI(QtWidgets.QWidget):
         self.flayout2_1.addRow(self.typeLabel, self.typeLineEdit)
 
         self.charView = QChartView(self)
-        self.showButton = QtWidgets.QPushButton(self)
+        self.showButton = QtWidgets.QPushButton('SHOW', self)
         self.vlayout2_2.addWidget(self.charView)
         self.vlayout2_2.addWidget(self.showButton)
+
+        self.runButton.clicked.connect(self.start_testbench)
+    # slot
+    def start_testbench(self):
+        # parse command
+        command, arguments, watchInterval = self.tool_parse_command()
+
+        # start a new thread to run testbench and watching
+        watchProcessControl = WatchProcessControl(watchInterval, self)
+        watchProcessControl.moveToThread(self.testbenchThread)
+
+        self.testbenchThread.finished.connect(watchProcessControl.deleteLater)
+        self.startTestbench.connect(watchProcessControl.start_testbench)
+        watchProcessControl.resultReady.connect(self.update_stat)
+        print(type(arguments))
+        self.startTestbench.emit(command, arguments)
+
+    def update_stat(self, info: dict):
+        pass
+
+    def tool_parse_command(self):
+        content = self.inputCommandLineEdit.text()
+        try:
+            watchInterval = int(self.testIntervalLineEdit.text())
+            command = content.split()[0]
+            arguments = content.split()[1:]
+            return command, arguments, watchInterval
+        except:
+            print("ERROR: command error!")
+
+
 
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    mainWindow = WatchUI()
-    mainWindow.show()
-    
-    # watchProcessControl  = WatchProcessControl()
-    # watchProcessControl.start_testbench('sysbench', ['memory', 'run'])
+    # mainWindow = WatchUI()
+    # mainWindow.show()
+    watchProcessControl  = WatchProcessControl(1000)
+    watchProcessControl.start_testbench('sysbench', ['memory', 'run'])
 
     # testProcess = QProcess(app)
     # testProcess.setStandardOutputFile('/proc/watch')
     # testProcess.start('echo', ['1'])
     # testProcess.waitForFinished()
-    # print(eval(str(readStatProcess.readAll(), 'utf-8')))
+    
     sys.exit(app.exec_())
     
